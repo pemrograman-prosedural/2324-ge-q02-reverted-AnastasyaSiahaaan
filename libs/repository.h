7b9baf464c04bf87103ad75187e8c8403c4b7b6a
AnastasyaTabitaBlessingSiahaan <anastasyasiahaan06@gmail.com>
2024-05-11 11:54:40 +0700

    pair 4

---
+++ libs/repository.h
@@ -0,0 +1,16 @@
+#ifndef REPOSITORY_H
+#define REPOSITORY_H
+
+#include "gender.h"
+#include "dorm.h"
+#include "student.h"
+
+/**
+ * @brief define your structure, enums, globally accessible variables, and function prototypes here.
+ * The actual function implementation should be defined in the corresponding source file.
+ *
+ */
+
+short line_count(char* fileName);
+
+#endif
